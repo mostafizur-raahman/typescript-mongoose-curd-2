23a30e490f4d938c721b63f951f090426788eb4c
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2023-11-27 16:24:34 +0600

    schema and model create done

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,78 +1,29 @@
-import mongoose from "mongoose";
+export type FullName = {
+    firstName: string;
+    lastName: string;
+};
 
-const userSchema = new mongoose.Schema({
-    userId: {
-        type: Number,
-        required: true,
-        unique: true,
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    fullName: {
-        firstName: {
-            type: String,
-            required: true,
-        },
-        lastName: {
-            type: String,
-            required: true,
-        },
-    },
-    age: {
-        type: Number,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    isActive: {
-        type: Boolean,
-        default: true,
-    },
-    hobbies: {
-        type: [String],
-        default: [],
-    },
-    address: {
-        street: {
-            type: String,
-            required: true,
-        },
-        city: {
-            type: String,
-            required: true,
-        },
-        country: {
-            type: String,
-            required: true,
-        },
-    },
-    orders: [
-        {
-            productName: {
-                type: String,
-                required: true,
-            },
-            price: {
-                type: Number,
-                required: true,
-            },
-            quantity: {
-                type: Number,
-                required: true,
-            },
-        },
-    ],
-});
+export type Address = {
+    street: string;
+    city: string;
+    country: string;
+};
 
-const User = mongoose.model("User", userSchema);
+export type Order = {
+    productName: string;
+    price: number;
+    quantity: number;
+};
 
-export default User;
+export type User = {
+    userId: number;
+    username: string;
+    password: string;
+    fullName: FullName;
+    age: number;
+    email: string;
+    isActive: boolean;
+    hobbies: string[];
+    address: Address;
+    orders: Order[];
+};
